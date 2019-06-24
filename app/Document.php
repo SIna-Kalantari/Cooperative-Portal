6d1bda691d0d547a125efd9b274e306dc60bc284
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-06-24 14:15:29 +0430

    edit the app/Document.php

--- app/Document.php
+++ app/Document.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $table = 'documents';
+    
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title' ,'type', 'destination', 'projectId', 'created_at'
+    ];
+
+    public function project(){
+        return $this->belongsTo('App\Project', 'projectId');
+    }
 }
