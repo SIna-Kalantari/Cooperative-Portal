110852c2f0453f886d61257eab22d520233a2bc0
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-07-03 12:40:42 +0430

    added relation

--- app/ProjectTechnology.php
+++ app/ProjectTechnology.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectTechnology extends Model
 {
-    //
+    protected $table = 'project_technologies';
+    
+    public $timestamps = false;
+
+    protected $fillable = [
+        'projectId', 'technologyId'
+    ];
+
+
+    public function project(){
+        return $this->belongsTo('App\Project');
+    }
+
+    public function technology(){
+        return $this->belongsTo('App\Technology');
+    }
 }
