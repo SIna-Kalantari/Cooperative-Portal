b30edf3b6b1d4ae36d77917199d47b023a35797f
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-07-06 13:02:52 +0430

    added use softDeletes to the model

--- app/UserProject.php
+++ app/UserProject.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserProject extends Model
 {
+    use SoftDeletes;
     protected $table = 'user_projects';
     
     public $timestamps = false;
