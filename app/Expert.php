67d779cd6730d1cbc6f1fe87a283e99d4d6b4c00
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-07-06 10:34:13 +0430

    edited app/Expert.php

--- app/Expert.php
+++ app/Expert.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expert extends Model
 {
-    //
+    protected $table = 'experts';
+    
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title'
+    ];
+
+    public function projectExpert(){
+        return $this->hasMany('App\Expert', 'projectId', 'expertId');
+    }
 }
