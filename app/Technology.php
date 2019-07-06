4cf2301377f569b3d79101c64c4f5fe7c65b2f3f
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-07-06 13:01:06 +0430

    added use softDeletes to the model

--- app/Technology.php
+++ app/Technology.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Technology extends Model
 {
+    use SoftDeletes;
     protected $table = 'technologies';
     
     public $timestamps = false;
